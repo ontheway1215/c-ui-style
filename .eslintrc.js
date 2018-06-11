e0ae6ac46da1f91ec3b00bf4e3cd328de4b1685c
ontheway1215 <179023084@qq.com>
2018-06-11 14:54:47 +0800

    add project config

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  extends: 'eslint:recommended',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  'rules': {
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'prefer-spread': 'off',
+    'class-methods-use-this': 'off'
+  }
+}
